f8abc4fc43a688883dced763a63bafea5dba1c01
birknejc <birknejc@yahoo.com>
2024-08-12 15:44:06 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.Extensions.Configuration;
 using MovieRental.Data;
 using MovieRental.DBContext;  // For AppDbContext
 using MovieRental.Services;   // For MovieService and OrderService
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,21 +13,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<CartService>();
-
-// Register the MovieService
+builder.Services.AddScoped<CartService>();
 builder.Services.AddScoped<MovieService>();
-
-// Register the MovieService
 builder.Services.AddScoped<OrderService>();
 
-//Register the CartService
-builder.Services.AddScoped<CartService>();
-
-
 // Connect directly to the SQL Server database
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=VideoRental;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+
+// Connect to PostgreSQL database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=VideoRental;Trusted_Connection=True;TrustServerCertificate=True;"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
+++ obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3064996a924119ee2426429d79991558ee499523")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
