4da5661f043ba538cd50de3c8d3a4d21597d5d53
birknejc <birknejc@yahoo.com>
2025-01-04 13:19:30 -0600

    Removed TestProject1

--- obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
+++ obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09309a40f72f8799a22dae1d4ad1d7d7d43a675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a144b69e17c9b4a320112f25bdab8bb00eb7658d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
