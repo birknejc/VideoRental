5fe6eed64e4a6fd46be7fa5dbaaea4d157e3fe2b
birknejc <birknejc@yahoo.com>
2025-01-20 12:00:29 -0600

    ConnectionUpdate

--- obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
+++ obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a144b69e17c9b4a320112f25bdab8bb00eb7658d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da5661f043ba538cd50de3c8d3a4d21597d5d53")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
