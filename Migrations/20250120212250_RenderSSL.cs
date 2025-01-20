48f3303149ce4260ce3c89d3e6dbb1d6e4dd9c32
birknejc <birknejc@yahoo.com>
2025-01-20 15:23:59 -0600

    RendorSSL

---
+++ Migrations/20250120212250_RenderSSL.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenderSSL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
+++ obj/Debug/net7.0/MovieRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da5661f043ba538cd50de3c8d3a4d21597d5d53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe6eed64e4a6fd46be7fa5dbaaea4d157e3fe2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
